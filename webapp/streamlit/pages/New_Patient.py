27d60d1cbd19ce41c352a8051163cb2a373adb34
SasikaA073 <sasikapamith2016@gmail.com>
2023-05-06 00:08:11 +0530

    ml model connected

--- webapp/streamlit/pages/New_Patient.py
+++ webapp/streamlit/pages/New_Patient.py
@@ -2,10 +2,14 @@ import streamlit as st
 from classes import Patient
 from database import *
 import pickle
+import datetime
 
 # Load ML model
 ml_model_name = "./ml_models/month1_model.pkl"
-model = pickle.load(open(ml_model_name, 'rb'))
+ml_model = pickle.load(open(ml_model_name, 'rb'))
+
+# A dictionary to map the output of the model to the risk level
+risk_level_dict = {1:"Low risk", 2:"Medium risk", 3:"High risk"}
 
 st.set_page_config(
     page_title="PregMa - Mother's Health Monitoring System", 
@@ -44,19 +48,38 @@ with st.form(key='reg_form'):
         st.write("Blood Group: ", blood_group)
         st.write("Mobile Number: ", mobile_number)
 
+        # Predict Data
+        X_data = (age, systolicBP, diastolicBP, blood_sugar, body_temp, heart_rate)
+        pred = ml_model.predict([X_data])
+        prediction = risk_level_dict[pred[0]]
+        
+        if pred[0] == 0:
+            st.success("Low Risk")
+        elif pred[0] == 1:
+            st.warning("Medium Risk")
+        elif pred[0] == 2:
+            st.error("High Risk")
+
+
+        st.write("Prediction: ", prediction)
+
+        # Get the current date
+        
+        date = datetime.datetime.now().date()
+
         # write to the database
         month_data = {systolicBP, diastolicBP, blood_sugar, body_temp, heart_rate}
-        insert_patient(patient_id, first_name, last_name, nic, age, blood_group, mobile_number, month, month_data)
+        insert_patient(patient_id, first_name, last_name, nic, age, blood_group, mobile_number, month, systolicBP, diastolicBP, blood_sugar, body_temp, heart_rate, prediction, date)
 
         st.success(f"{patient_id} Mother's Month {month} results updated Successfully".format(first_name))
         # st.balloons()
-        st.write("Systolic Blood Pressure: ")
-        st.write("Systolic Blood Pressure: ", systolicBP)   
-        st.write("Diastolic Blood Pressure: ", diastolicBP)
-        st.write("Blood Sugar Level: ", blood_sugar)
-        st.write("Body Temperature (in Celsius): ", body_temp)
-        st.write("Heart Rate (in BPM): ", heart_rate)
-        st.write("Month: ", month)
+        # st.write("Systolic Blood Pressure: ")
+        # st.write("Systolic Blood Pressure: ", systolicBP)   
+        # st.write("Diastolic Blood Pressure: ", diastolicBP)
+        # st.write("Blood Sugar Level: ", blood_sugar)
+        # st.write("Body Temperature (in Celsius): ", body_temp)
+        # st.write("Heart Rate (in BPM): ", heart_rate)
+        # st.write("Month: ", month)
 
         
     
