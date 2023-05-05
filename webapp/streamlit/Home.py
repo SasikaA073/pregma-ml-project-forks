3b9155d153c5f1598a8da49b310fafe1a52cfdc1
Santhusha <santhushajanana@gmail.com>
2023-05-05 14:27:01 +0530

    streamlit update

--- webapp/streamlit/Home.py
+++ webapp/streamlit/Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 # import streamlit_authenticator as stauth
 # import yaml
 # from yaml.loader import SafeLoader
--- webapp/streamlit/pages/Dashboard.py
+++ webapp/streamlit/pages/Dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title="PregMa - Mother's Health Monitoring System", 
@@ -12,7 +13,7 @@ st.title("Patient's Dashboard")
 
 
 # Define the column names
-columns = ['Patient ID', 'NIC', "Mother's name", 'Mobile Number', 'Month', 'Predicted Risk level']
+columns_1 = ['Patient ID', 'NIC', "Mother's name", 'Mobile Number', 'Month', 'Predicted Risk level']
 
 # Create a sample data frame
 data = {
@@ -27,10 +28,53 @@ data = {
 df = pd.DataFrame(data)
 
 # Display the table
-st.table(df[columns])
+st.table(df[columns_1])
 
+st.divider()
 
+st.subheader("Mother {}'s Health Status")
 
+# Define the columns for the table
+columns = ['Patient\'s ID', 'NIC', 'First name', 'Last name', 'Mobile Number', 'Month', 'Current predicted Risk Level']
 
+# Define a function to get the data for the table
+def get_data():
+    return [{'Patient\'s ID': '1', 'NIC': '1234567890', 'First name': 'John', 'Last name': 'Doe', 'Mobile Number': '1234567890', 'Month': 5, 'Current predicted Risk Level': 'High'}]
 
+# Display the table
+st.table(pd.DataFrame(get_data(), columns=columns))
 
+st.subheader("Current Health Report")
+# Define the columns for the table
+columns = ['Month', 'Predicted Status', 'Actual Status']
+
+# Define the data for the table
+data = [['1', 'Low Risk', 'Low Risk'],
+    ['2', 'Low Risk', 'Low Risk'],
+    ['3', 'Low Risk', 'Low Risk'],
+    ['4', 'High Risk', 'High Risk'],
+    ['5', 'Low Risk', 'Low Risk'],
+    ['6', 'Mid Risk', 'Mid Risk'],
+]
+
+data_plot = [['1', '0', '0.5'],
+    ['2', '0', '0'],
+    ['3', '0', '0'],
+    ['4', '1', '0.5'],
+    ['5', '0', '0'],
+    ['6', '0.5', '1'],
+]
+
+st.table(pd.DataFrame(data, columns=columns))
+
+# Display the table
+df = pd.DataFrame(data_plot, columns=columns)
+
+# Create a line chart for the "Predicted Status" column
+fig_pred = px.line(df, x='Month', y=['Predicted Status', 'Actual Status'])
+
+# Create a line chart for the "Actual Status" column
+# fig_act = px.line(df, x='Month', y='Actual Status')
+
+# Display the charts using streamlit.plotly_chart()
+st.plotly_chart(fig_pred)
