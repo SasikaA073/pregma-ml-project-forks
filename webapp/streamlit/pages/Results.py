51c0e14339a68ef053795a82a38ecbea6696064d
SasikaA073 <sasikapamith2016@gmail.com>
2023-05-07 04:01:23 +0530

    dsf

--- webapp/streamlit/pages/Results.py
+++ webapp/streamlit/pages/Results.py
@@ -37,6 +37,8 @@ st.set_page_config(
 
 st.title("User Profile")
 
+st.markdown("Optical Character Recognizer")
+
 image = st.file_uploader(label = "Upload your image here autofill or you can fill the form",type=['png','jpg','jpeg'])
 
 @st.cache_data
